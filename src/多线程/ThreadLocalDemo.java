cbd287650628f11d8e8da95175b40e6d33179edb
943785513@qq.com <943785513@qq.com>
2021-04-28 21:36:00 +0800

    手写Spring + Spring MVC

--- src/多线程/ThreadLocalDemo.java
+++ src/多线程/ThreadLocalDemo.java
@@ -1,4 +1,45 @@
 package 多线程;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadLocalDemo {
+    private static ThreadLocal<List<String>> threadLocal = new ThreadLocal<>();
+
+    public void setThreadLocal(List<String> value){
+        threadLocal.set(value);
+    }
+
+    public void getThreadLocal(){
+        threadLocal.get().forEach(name-> System.out.println(Thread.
+                currentThread().getName() + ":" + name));
+    }
+
+    public static void main(String[] args) {
+        // 这是全局的，放在堆里
+        final ThreadLocalDemo demo = new ThreadLocalDemo();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<String> strs = new ArrayList<>();
+                strs.add("1");
+                strs.add("2");
+                strs.add("3");
+                demo.setThreadLocal(strs);
+                demo.getThreadLocal();
+            }
+        }, "t1").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<String> strs = new ArrayList<>();
+                strs.add("a");
+                strs.add("b");
+                demo.setThreadLocal(strs);
+                demo.getThreadLocal();
+            }
+        }, "t2").start();
+    }
 }
